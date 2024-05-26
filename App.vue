16fc33466f60ecb915471800f1d0d94155e3cc9a
fubd <312bdf@gmail.com>
2024-05-26 14:43:38 +0800

    init

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    data() {
+      return {
+        count: 0
+      }
+    },
+
+    methods: {
+      onClick() {
+        this.count++
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <button @click="onClick">click, {{ count }}</button>
+  </div>
+</template>
